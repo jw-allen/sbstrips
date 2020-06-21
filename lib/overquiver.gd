49d8357a42da04fe4c3543ee4dd5e9bea6e123d7
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-06-21 18:21:34 +0100

    Update declarations

--- lib/overquiver.gd
+++ lib/overquiver.gd
@@ -1,1 +1,26 @@
-# CODE FOR THE OVERQUIVER WILL GO HERE
+DeclareProperty( "Is1RegularQuiver", IsQuiver );
+
+DeclareOperation( "QuiverOfQuiverPath", [ IsPath ] );
+
+DeclareAttribute( "1RegQuivIntActionFunction", IsQuiver );
+
+DeclareOperation( "1RegQuivIntAct" [ IsPath, IsInt ] );
+
+DeclareAttribute( "2RegAugmentationOfQuiver" IsQuiver );
+
+DeclareProperty( "Is2RegAugmentationOfSbQuiver", IsQuiver );
+
+DeclareAttribute( "OriginalSbQuiverOf2RegAugmentation", IsQuiver );
+
+DeclareAttribute( "RetractionOf2RegAugmentation", IsQuiver );
+
+DeclareAttribute( "CompatibleTrackPermutationOfSbAlg",
+ IsSpecialBiserialAlgebra );
+ 
+DeclareAttribute( "OverquiverOfSbAlg", IsSpecialBiserialAlgebra );
+
+DeclareProperty( "IsOverquiver", IsQuiver );
+
+DeclareAttribute( "SbAlgOfOverquiver", IsSpecialBiserialAlgebra );
+
+#########1#########2#########3#########4#########5#########6#########7#########

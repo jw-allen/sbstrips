4c0701f43e58f58c9890c3402d0ab4de8b3540b4
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-06-22 16:01:17 +0100

    declare VertexIndexedSequenceFamilyOfQuiver

--- lib/vertseqs.gd
+++ lib/vertseqs.gd
@@ -1,2 +1,6 @@
 DeclareRepresentation( "IsVertexIndexedSequenceRep", IsAttributeStoringRep,
  [ "indices", "kind_of_seq", "terms" ] );
+
+DeclareAttribute( "VertexIndexedSequenceFamilyOfQuiver", IsQuiver );
+
+#########1#########2#########3#########4#########5#########6#########7#########

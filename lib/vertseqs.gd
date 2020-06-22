e9359fef091acb263067e16dffb35cf796be6a90
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-06-22 22:30:28 +0100

    tested in GAP, it works!

--- lib/vertseqs.gd
+++ lib/vertseqs.gd
@@ -1,6 +1,8 @@
 DeclareRepresentation( "IsVertexIndexedSequenceRep", IsAttributeStoringRep,
- [ "indices", "kind_of_seq", "terms" ] );
+ [ "indices", "terms", "kind_of_seq" ] );
 
 DeclareAttribute( "VertexIndexedSequenceFamilyOfQuiver", IsQuiver );
+
+DeclareOperation( "VISify", [ IsQuiver, IsList, IsString ] );
 
 #########1#########2#########3#########4#########5#########6#########7#########

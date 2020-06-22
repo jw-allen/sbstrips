3112bbf6b21f224bef75eb42afd9068f6fc32bcc
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-06-22 14:23:47 +0100

    update declarations

--- lib/1reg.gd
+++ lib/1reg.gd
@@ -1,1 +1,11 @@
+DeclareProperty( "Is1RegQuiver", IsQuiver );
 
+DeclareAttribute( "1RegQuivIntActionFunction", IsQuiver );
+
+DeclareGlobalFunction( "1RegQuivIntAct" );
+
+DeclareGlobalFunction( "PathBySourceAndLength" );
+
+DeclareGlobalFunction( "PathByTargetAndLength" );
+
+#########1#########2#########3#########4#########5#########6#########7#########

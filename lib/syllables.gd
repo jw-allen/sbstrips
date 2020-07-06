d19317428436454bea49ce8282c3ab89f36602eb
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-07-06 18:01:24 +0100

    update syllables.gd

--- lib/syllables.gd
+++ lib/syllables.gd
@@ -1,1 +1,19 @@
+DeclareRepresentation( "IsSyllableRep", IsAttributeStoringRep,
+ [ "path", "perturbation", "sb_alg" ] );
+
+DeclareAttribute( "SyllableFamilyOfSbAlg", IsSpecialBiserialAlgebra );
+
+DeclareAttribute( "SyllableSetOfSbAlg", IsSpecialBiserialAlgebra );
+
+DeclareAttribute( "ZeroSyllableOfSbAlg", IsSpecialBiserialAlgebra );
+
+DeclareProperty( "IsZeroSyllable", IsSyllableRep );
+
+DeclareProperty( "IsStableSyllable", IsSyllableRep );
+
+DeclareOperation( "SbAlgOfSyllable", [ IsSyllableRep ] )
+
+DeclareAttribute( "DescentFunctionOfSbAlg", IsSpecialBiserialAlgebra );
+DeclareAttribute( "SidestepFunctionOfSbAlg", IsSpecialBiserialAlgebra );
+
 #########1#########2#########3#########4#########5#########6#########7#########

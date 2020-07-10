5f9ceed1a4b43df4a3426a7cf6658e5f7c790ef8
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-07-10 10:47:34 +0100

    declare IsPinBoundarySyllable

--- lib/syllables.gd
+++ lib/syllables.gd
@@ -2,25 +2,23 @@ DeclareRepresentation( "IsSyllableRep", IsAttributeStoringRep,
  [ "path", "perturbation", "sb_alg" ] );
 
 DeclareAttribute( "SyllableFamilyOfSbAlg", IsSpecialBiserialAlgebra );
-
 DeclareAttribute( "SyllableSetOfSbAlg", IsSpecialBiserialAlgebra );
+DeclareAttribute( "ZeroSyllableOfSbAlg", IsSpecialBiserialAlgebra );
 
 DeclareOperation( "Syllabify", [ IsPath, IsInt ] );
+
 DeclareOperation( "UnderlyingPathOfSyllable", [ IsSyllableRep ] );
 DeclareOperation( "PerturbationTermOfSyllable", [ IsSyllableRep ] );
 
-DeclareAttribute( "ZeroSyllableOfSbAlg", IsSpecialBiserialAlgebra );
-
 DeclareProperty( "IsZeroSyllable", IsSyllableRep );
-
 DeclareProperty( "IsStableSyllable", IsSyllableRep );
 DeclareProperty( "IsSyllableWithStableSource", IsSyllableRep );
+DeclareProperty( "IsUltimatelyDescentStableSyllable", IsSyllableRep );
+DeclareProperty( "IsPinBoundarySyllable", IsSyllableRep );
 
 DeclareOperation( "SbAlgOfSyllable", [ IsSyllableRep ] );
 
 DeclareAttribute( "DescentFunctionOfSbAlg", IsSpecialBiserialAlgebra );
 DeclareAttribute( "SidestepFunctionOfSbAlg", IsSpecialBiserialAlgebra );
-
-DeclareProperty( "IsUltimatelyDescentStableSyllable", IsSyllableRep );
 
 #########1#########2#########3#########4#########5#########6#########7#########

2ff70ed5360ba3b6c4d040d83d8a281fe0ca2d56
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-07-13 17:38:14 +0100

    create patchreps.gd

---
+++ patchreps.gd
@@ -0,0 +1,15 @@
+DeclareRepresentation( "IsPatchRep", IsAttributeStoringRep,
+ [ "NW", "NE", "SW", "SE" ]
+ );
+
+DeclareAttribute( "IsZeroPatch", IsPatchRep );
+DeclareAttribute( "IsPatchOfStringProjective", IsPatchRep );
+DeclareAttribute( "IsPatchOfPinModule", IsPatchRep );
+DeclareAttribute( "IsVirtualPatch", IsPatchRep );
+DeclareAttribute( "ReflectionOfPatch", IsPatchRep );
+
+DeclareOperation( "Patchify",
+ [ IsPatchRep, IsPatchRep, IsPatchRep, IsPatchRep ]
+ );
+
+#########1#########2#########3#########4#########5#########6#########7#########

5f6e2fc832d73445c5d11526d9b3203a430e37ad
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-07-16 16:41:03 +0100

    declare IsStripRep and StripFamilyOfSbAlg

---
+++ lib/strips.gd
@@ -0,0 +1,5 @@
+DeclareRepresentation( "IsStripRep", IsPositionalObjectRep, [] );
+
+DeclareAttribute( "StripFamilyOfSbAlg", IsSpecialBiserialAlgebra );
+
+#########1#########2#########3#########4#########5#########6#########7#########

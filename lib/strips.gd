61d622a2a99b0f1fbd368ca782f1791e94750617
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-07-17 17:11:32 +0100

    declare wishlist of attributes (to be implemented)
    namely the attributes SimpleStripsOfSbAlg, ProjectiveStripsOfSbAlg and InjectiveStripsOfSbAlg of IsSpecialBiserialAlgebra

--- lib/strips.gd
+++ lib/strips.gd
@@ -7,4 +7,8 @@ DeclareGlobalFunction( "StripifyFromSyllablesAndOrientationsNC" );
 DeclareGlobalFunction( "StripifyFromSbAlgPathNC" );
 DeclareGlobalFunction( "Stripify" );
 
+DeclareAttribute( "SimpleStripsOfSbAlg", IsSpecialBiserialAlgebra );
+DeclareAttribute( "ProjectiveStripsOfSbAlg", IsSpecialBiserialAlgebra );
+DeclareAttribute( "InjectiveStripsOfSbAlg", IsSpecialBiserialAlgebra );
+
 #########1#########2#########3#########4#########5#########6#########7#########

5dc3119f184e2234b02f8b901140e9de1c449dec
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-07-17 10:48:56 +0100

    declare NC functions for Stripify to call
    declare the global functions StripifyFromSyllablesAndOrientationsNC or StripifyFromSbAlgPathNC

--- lib/strips.gd
+++ lib/strips.gd
@@ -3,6 +3,8 @@ DeclareRepresentation( "IsStripRep", IsPositionalObjectRep, [] );
 DeclareAttribute( "StripFamilyOfSbAlg", IsSpecialBiserialAlgebra );
 DeclareAttribute( "ZeroStripOfSbAlg", IsSpecialBiserialAlgebra );
 
+DeclareGlobalFunction( "StripifyFromSyllablesAndOrientationsNC" );
+DeclareGlobalFunction( "StripifyFromSbAlgPathNC" );
 DeclareGlobalFunction( "Stripify" );
 
 #########1#########2#########3#########4#########5#########6#########7#########

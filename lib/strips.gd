42a23874f62fac1a5f7dc31aa97ffff873539b06
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-07-17 10:23:09 +0100

    declare Stripify global function

--- lib/strips.gd
+++ lib/strips.gd
@@ -3,4 +3,6 @@ DeclareRepresentation( "IsStripRep", IsPositionalObjectRep, [] );
 DeclareAttribute( "StripFamilyOfSbAlg", IsSpecialBiserialAlgebra );
 DeclareAttribute( "ZeroStripOfSbAlg", IsSpecialBiserialAlgebra );
 
+DeclareGlobalFunction( "Stripify" );
+
 #########1#########2#########3#########4#########5#########6#########7#########

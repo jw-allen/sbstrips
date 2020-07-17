d2385fe71e128b65298ea3908f4995c0ecd1be6c
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-07-17 10:17:49 +0100

    declare ZeroStripOfSbAlg attribute

--- lib/strips.gd
+++ lib/strips.gd
@@ -1,5 +1,6 @@
 DeclareRepresentation( "IsStripRep", IsPositionalObjectRep, [] );
 
 DeclareAttribute( "StripFamilyOfSbAlg", IsSpecialBiserialAlgebra );
+DeclareAttribute( "ZeroStripOfSbAlg", IsSpecialBiserialAlgebra );
 
 #########1#########2#########3#########4#########5#########6#########7#########

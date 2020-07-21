edefbbf0c53ad72240e7cb062b1d9b9e781d9af1
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-07-21 18:33:39 +0100

    formatting; declare SbAlgResidueOfOverquiverPathNC

--- lib/overquiver.gd
+++ lib/overquiver.gd
@@ -1,21 +1,17 @@
 DeclareAttribute( "2RegAugmentationOfQuiver", IsQuiver );
-
 DeclareProperty( "Is2RegAugmentationOfQuiver", IsQuiver );
-
 DeclareAttribute( "OriginalSbQuiverOf2RegAugmentation", IsQuiver );
-
 DeclareAttribute( "RetractionOf2RegAugmentation", IsQuiver );
 
 DeclareAttribute( "CompatibleTrackPermutationOfSbAlg",
  IsSpecialBiserialAlgebra );
  
 DeclareAttribute( "OverquiverOfSbAlg", IsSpecialBiserialAlgebra );
-
 DeclareProperty( "IsOverquiver", IsQuiver );
-
 DeclareAttribute( "ContractionOfOverquiver", IsQuiver );
-
 DeclareAttribute( "SbAlgOfOverquiver", IsQuiver );
+
+DeclareGlobalFunction( "SbAlgResidueOfOverquiverPathNC" );
 
 DeclareAttribute( "ExchangePartnerOfVertex", IsQuiverVertex );
 

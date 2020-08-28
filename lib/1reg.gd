c56a0d4926891377b70fa5aa4e60a3e441fd7921
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-08-28 15:09:39 +0100

    document 1RegQuivIntActionFunction

--- lib/1reg.gd
+++ lib/1reg.gd
@@ -14,6 +14,29 @@
 ##  <#/GAPDoc>
 DeclareProperty( "Is1RegQuiver", IsQuiver );
 
+##  <#GAPDoc Label="Doc1RegQuivIntActionFunction">
+##  <ManSection>
+##    <Attr Name="Is1RegQuiver" Arg="quiver">
+##      <Description>
+##        Argument: <A>quiver</A>, a <M>1</M>-regular quiver (as tested by
+##        <Ref Prop="DocIs1RegQuiver"/>)
+##        <Br />
+##      </Description>
+##      <Returns>
+##        a single function <C>f</C> describing the <M>&ZZ;</M>-actions on the
+##        vertices and the arrows of <A>quiver</A>
+##      </Returns>
+##      <Description>
+##         Recall that a quiver is <M>1</M>-regular iff the source and target
+##         functions <M>s,t</M> are bijections from the arrow set to the vertex
+##         set (in which case the inverse <M>t^{-1}</M> is well-defined). The
+##         generator <M>1 \in &ZZ;</M> acts as ``<M>t^{-1}</M> then <M>s</M>''
+##         on vertices and ``<M>s</M> then <M>t^{-1}</M>'' on arrows.
+##         <Br />
+##      </Description>
+##    </Attr>
+##  </ManSection>
+##  <#/GAPDoc>
 DeclareAttribute( "1RegQuivIntActionFunction", IsQuiver );
 
 DeclareOperation( "1RegQuivIntAct", [ IsPath, IsInt ] );

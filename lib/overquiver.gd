ee7bbe206fa6f4b49c223fae88790c2f886e9454
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-08-28 16:38:35 +0100

    document Is2RegAugmentationOfQuiver

--- lib/overquiver.gd
+++ lib/overquiver.gd
@@ -1,4 +1,43 @@
+##  <#GAPDoc Label="Doc2RegAugmentationOfQuiver">
+##  <ManSection>
+##    <Attr Name="2RegAugmentationOfQuiver" Arg="ground_quiv">
+##      <Description>
+##        Argument: <A>ground_quiv</A>, a sub<M>2</M>-regular quiver (as tested
+##        by <Ref BookName="qpa" Prop="IsSpecialBiserialQuiver"/>)
+##        <Br />
+##      </Description>
+##      <Returns>
+##        a <M>2</M>-regular quiver of which <A>ground_quiv</A> may naturally
+##        be seen as a subquiver
+##      </Returns>
+##      <Description>
+##        If <A>ground_quiv</A> is itself sub-<M>2</M>-regular, then this
+##        attribute returns <A>ground_quiv</A> identically. If not, then this
+##        attribute constructs a brand new quiver object which has vertices
+##        and arrows having the same names as those of <A>ground_quiv</A>, but
+##        also has arrows with names <C>augarr1</C>, <C>augarr2</C> and so on.
+##        <Br />
+##      </Description>
+##    </Attr>
+##  </ManSection>
+##  <#/GAPDoc>
 DeclareAttribute( "2RegAugmentationOfQuiver", IsQuiver );
+
+##  <#GAPDoc Label="DocIs2RegAugmentationOfQuiver">
+##  <ManSection>
+##    <Prop Name="Is2RegAugmentationOfQuiver" Arg="quiver">
+##      <Description>
+##        Argument: <A>quiver</A>, a quiver
+##        <Br />
+##      </Description>
+##      <Returns>
+##        &true; if <A>quiver</A> was constructed by
+##        <Ref Label="Doc2RegAugmentationOfQuiver"/> or if <A>quiver</A> was an
+##        already <M>2</M>-regular quiver, and &false; otherwise.
+##      </Returns>
+##    </Prop>
+##  </ManSection>
+##  <#/GAPDoc>
 DeclareProperty( "Is2RegAugmentationOfQuiver", IsQuiver );
 DeclareAttribute( "OriginalSbQuiverOf2RegAugmentation", IsQuiver );
 DeclareAttribute( "RetractionOf2RegAugmentation", IsQuiver );

1d5185c94aed0b41e44179b1f52643ade55d52a4
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-09-07 09:27:38 +0100

    declare IsPanelRep

--- lib/panels.gd
+++ lib/panels.gd
@@ -1,1 +1,12 @@
-# Declaration file for panels
+##  <#GAPDoc Label="DocIsPanelRep">
+##  <ManSection>
+##    <Filt Name="IsPanelRep" Type="Representation">
+##      <Description>
+##        Panels in &sbstrips; belong to this representation filter.
+##      </Description>
+##    </Filt>
+##  </ManSection>
+##  <#/GAPDoc>
+DeclareRepresentation( "IsPanelRep", IsAttributeStoringRep, [ "panel" ] );
+
+#########1#########2#########3#########4#########5#########6#########7#########

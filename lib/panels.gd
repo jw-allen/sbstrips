46e021aa47b0ab0ec98d41d45429cfb9bcd557f3
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-09-07 09:41:06 +0100

    declare StationaryPanelsOfSbAlg

--- lib/panels.gd
+++ lib/panels.gd
@@ -11,4 +11,22 @@ DeclareRepresentation( "IsPanelRep", IsAttributeStoringRep, [ "panel" ] );
 
 DeclareOperation( "PanellifyNC", [ IsList ] );
 
+##  <#GAPDoc Label="DocStationaryPanelsOfSbAlg">
+##  <ManSection>
+##    <Attr Name="StationaryPanelsOfSbAlg" Arg="sba">
+##      <Description>
+##        Argument: <A>sba</A>, a special biserial algebra
+##      </Description>
+##      <Returns>
+##        the stationary panels of <A>sba</A>; that is, the list of panels
+##        corresponding to the stationary syllables of <A>sba</A>.
+##      </Returns>
+##      <Description>
+##        These are what control whether the <A>sba</A> is syzygy-finite.
+##      </Description>
+##    </Attr>
+##  </ManSection>
+##  <#/GAPDoc>
+DeclareAttribute( "StationaryPanelsOfSbAlg", IsSpecialBiserialAlgebra );
+
 #########1#########2#########3#########4#########5#########6#########7#########

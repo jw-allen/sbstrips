360a725f582b0609915bfcd77fad4c11b3fa78a3
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-10-08 12:21:28 +0100

    declare MakeSBStripsDocumentation

---
+++ lib/makesbstripsdoc.gd
@@ -0,0 +1,5 @@
+##  A convenient function that (re)makes all the SBStrips documentation on my
+##   computer
+DeclareGlobalFunction( "MakeSBStripsDocumentation" );
+
+#########1#########2#########3#########4#########5#########6#########7#########

af995e3cec6d835cc3a75f7bb8faea12f907c50b
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-10-09 12:08:28 +0100

    Func-like elements should be empty
    You don't write "<Attr Name="blah" Arg="x"> content </Attr>", you just write "<Attr Name="blah" Arg="x"/>". Now I know

--- lib/1reg.gd
+++ lib/1reg.gd
@@ -1,104 +1,99 @@
 ##  <#GAPDoc Label="DocIs1RegQuiver">
 ##  <ManSection>
-##    <Prop Name="Is1RegQuiver" Arg="quiver">
-##      <Description>
-##        Argument: <A>quiver</A>, a quiver
-##        <Br />
-##      </Description>
-##      <Returns>
-##        either <C>true</C> or <Code>false</Code>, depending on whether or not
-##        <A>quiver</A> is <M>1</M>-regular.
-##      </Returns>
-##    </Prop>
+##    <Prop Name="Is1RegQuiver" Arg="quiver"/>
+##    <Description>
+##      Argument: <A>quiver</A>, a quiver
+##      <Br />
+##    </Description>
+##    <Returns>
+##      either <C>true</C> or <Code>false</Code>, depending on whether or not
+##      <A>quiver</A> is <M>1</M>-regular.
+##    </Returns>
 ##  </ManSection>
 ##  <#/GAPDoc>
 DeclareProperty( "Is1RegQuiver", IsQuiver );
 
 ##  <#GAPDoc Label="Doc1RegQuivIntActionFunction">
 ##  <ManSection>
-##    <Attr Name="1RegQuivIntActionFunction" Arg="quiver">
-##      <Description>
-##        Argument: <A>quiver</A>, a <M>1</M>-regular quiver (as tested by
-##        <Ref Prop="DocIs1RegQuiver"/>)
-##        <Br />
-##      </Description>
-##      <Returns>
-##        a single function <C>f</C> describing the <M>&ZZ;</M>-actions on the
-##        vertices and the arrows of <A>quiver</A>
-##      </Returns>
-##      <Description>
-##        Recall that a quiver is <M>1</M>-regular iff the source and target
-##        functions <M>s,t</M> are bijections from the arrow set to the vertex
-##        set (in which case the inverse <M>t^{-1}</M> is well-defined). The
-##        generator <M>1 \in &ZZ;</M> acts as ``<M>t^{-1}</M> then <M>s</M>''
-##        on vertices and ``<M>s</M> then <M>t^{-1}</M>'' on arrows.
-##        <Br />
-##        In practice you will probably want to use
-##        <Ref Oper="Doc1RegQuivIntAct"/>, since it saves you having to remind
-##        &sbstrips; which quiver you intend to act on.
-##      </Description>
-##    </Attr>
+##    <Attr Name="1RegQuivIntActionFunction" Arg="quiver"/>
+##    <Description>
+##      Argument: <A>quiver</A>, a <M>1</M>-regular quiver (as tested by
+##      <Ref Prop="DocIs1RegQuiver"/>)
+##      <Br />
+##    </Description>
+##    <Returns>
+##      a single function <C>f</C> describing the <M>&ZZ;</M>-actions on the
+##      vertices and the arrows of <A>quiver</A>
+##    </Returns>
+##    <Description>
+##      Recall that a quiver is <M>1</M>-regular iff the source and target
+##      functions <M>s,t</M> are bijections from the arrow set to the vertex
+##      set (in which case the inverse <M>t^{-1}</M> is well-defined). The
+##      generator <M>1 \in &ZZ;</M> acts as ``<M>t^{-1}</M> then <M>s</M>''
+##      on vertices and ``<M>s</M> then <M>t^{-1}</M>'' on arrows.
+##      <Br />
+##      In practice you will probably want to use
+##      <Ref Oper="Doc1RegQuivIntAct"/>, since it saves you having to remind
+##      &sbstrips; which quiver you intend to act on.
+##    </Description>
 ##  </ManSection>
 ##  <#/GAPDoc>
 DeclareAttribute( "1RegQuivIntActionFunction", IsQuiver );
 
 ##  <#GAPDoc Label="Doc1RegQuivIntAct">
 ##  <ManSection>
-##    <Oper Name="1RegQuivIntAct" Arg="x, k">
-##      <Description>
-##        Arguments: <A>x</A>, which is either a vertex or an arrow of a
-##        <M>1</M>-regular quiver; <A>k</A>, an integer.
-##        <Br />
-##      </Description>
-##      <Returns>
-##        the path <M>x+k</M>, as per the <M>&ZZ;</M>-action (see below).
-##      </Returns>
-##      <Description>
-##        Recall that a quiver is <M>1</M>-regular iff the source and target
-##        functions <M>s,t</M> are bijections from the arrow set to the vertex
-##        set (in which case the inverse <M>t^{-1}</M> is well-defined). The
-##        generator <M>1 \in &ZZ;</M> acts as ``<M>t^{-1}</M> then <M>s</M>''
-##        on vertices and ``<M>s</M> then <M>t^{-1}</M>'' on arrows.
-##        <Br />
-##        This operation figures out from <A>x</A> the quiver to which <A>x</A>
-##        belongs and applies <Ref Attr="Doc1RegQuivIntActionFunction"> of that
-##        quiver. For this reason, it is more user-friendly.
-##      </Description>
-##    </Oper>
+##    <Oper Name="1RegQuivIntAct" Arg="x, k"/>
+##    <Description>
+##      Arguments: <A>x</A>, which is either a vertex or an arrow of a
+##      <M>1</M>-regular quiver; <A>k</A>, an integer.
+##      <Br />
+##    </Description>
+##    <Returns>
+##      the path <M>x+k</M>, as per the <M>&ZZ;</M>-action (see below).
+##    </Returns>
+##    <Description>
+##      Recall that a quiver is <M>1</M>-regular iff the source and target
+##      functions <M>s,t</M> are bijections from the arrow set to the vertex
+##      set (in which case the inverse <M>t^{-1}</M> is well-defined). The
+##      generator <M>1 \in &ZZ;</M> acts as ``<M>t^{-1}</M> then <M>s</M>''
+##      on vertices and ``<M>s</M> then <M>t^{-1}</M>'' on arrows.
+##      <Br />
+##      This operation figures out from <A>x</A> the quiver to which <A>x</A>
+##      belongs and applies <Ref Attr="Doc1RegQuivIntActionFunction"/> of
+##      that quiver. For this reason, it is more user-friendly.
+##    </Description>
 ##  </ManSection>
 ##  <#/GAPDoc>  
 DeclareOperation( "1RegQuivIntAct", [ IsPath, IsInt ] );
 
 ##  <#GAPDoc Label="DocPathBySourceAndLength">
 ##  <ManSection>
-##    <Oper Name="PathBySourceAndLength" Arg="vert, len">
-##      <Description>
-##        Arguments: <A>vert</A>, a vertex of a <M>1</M>-regular quiver
-##        <M>Q</M>; <A>len</A>, a nonnegative integer.
-##        <Br />
-##      </Description>
-##      <Returns>
-##        the unique path in <M>Q</M> which has source <A>vert</A> and length
-##        <A>len</A>.
-##      </Returns>
-##    </Oper>
+##    <Oper Name="PathBySourceAndLength" Arg="vert, len"/>
+##    <Description>
+##      Arguments: <A>vert</A>, a vertex of a <M>1</M>-regular quiver
+##      <M>Q</M>; <A>len</A>, a nonnegative integer.
+##      <Br />
+##    </Description>
+##    <Returns>
+##      the unique path in <M>Q</M> which has source <A>vert</A> and length
+##      <A>len</A>.
+##    </Returns>
 ##  </ManSection>
 ##  <#/GAPDoc>  
 DeclareOperation( "PathBySourceAndLength", [ IsQuiverVertex, IsInt ] );
 
 ##  <#GAPDoc Label="DocPathByTargetAndLength">
 ##  <ManSection>
-##    <Oper Name="PathByTargetAndLength" Arg="vert, len">
-##      <Description>
-##        Arguments: <A>vert</A>, a vertex of a <M>1</M>-regular quiver
-##        <M>Q</M>; <A>len</A>, a nonnegative integer.
-##        <Br />
-##      </Description>
-##      <Returns>
-##        the unique path in <M>Q</M> which has target <A>vert</A> and length
-##        <A>len</A>.
-##      </Returns>
-##    </Oper>
+##    <Oper Name="PathByTargetAndLength" Arg="vert, len"/>
+##    <Description>
+##      Arguments: <A>vert</A>, a vertex of a <M>1</M>-regular quiver
+##      <M>Q</M>; <A>len</A>, a nonnegative integer.
+##      <Br />
+##    </Description>
+##    <Returns>
+##      the unique path in <M>Q</M> which has target <A>vert</A> and length
+##      <A>len</A>.
+##    </Returns>
 ##  </ManSection>
 ##  <#/GAPDoc>  
 DeclareOperation( "PathByTargetAndLength", [ IsQuiverVertex, IsInt ] );

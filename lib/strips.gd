8ec762aaeaa4807229fad895817b7667c4cb62b4
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-10-09 12:30:01 +0100

    Func-like GAPDoc elements should be empty

--- lib/strips.gd
+++ lib/strips.gd
@@ -26,16 +26,15 @@ DeclareOperation(
 
 ##  <#GAPDoc Label="DocWidthOfStrip">
 ##    <ManSection>
-##      <Oper Name="WidthOfStrip" Arg="strip">
-##        <Description>
-##          Argument: <A>strip</A>, a strip
-##          <Br />
-##        </Description>
-##        <Returns>
-##          a nonnegative integer, counting the number (with multiplicity) of
-##          syllables of <A>strip</A> are nonstationary.
-##        </Returns>
-##      </Oper>
+##    <Oper Name="WidthOfStrip" Arg="strip"/>
+##      <Description>
+##        Argument: <A>strip</A>, a strip
+##        <Br />
+##      </Description>
+##      <Returns>
+##        a nonnegative integer, counting the number (with multiplicity) of
+##        syllables of <A>strip</A> are nonstationary.
+##      </Returns>
 ##    </ManSection>
 ##  <#/GAPDoc>
 
@@ -43,21 +42,19 @@ DeclareOperation( "WidthOfStrip", [ IsStripRep ] );
 
 ##  <#GAPDoc Label="DocIsZeroStrip">
 ##    <ManSection>
-##      <Prop Name="IsZeroStrip" Arg="strip">
-##        <Description>
-##          Argument: <A>strip</A>, a strip
-##          <Br />
-##        </Description>
-##        <Returns>
-##          &true; if <A>strip</A> is the zero strip of some SB algebra,
-##          and &false; otherwise.
-##        </Returns>
-##        <Description>
-##          Note that &sbstrips; knows which SB algebra <A>strip</A> belongs
-##          to.
-##          <Br />
-##        </Description>
-##      </Prop>
+##      <Prop Name="IsZeroStrip" Arg="strip"/>
+##      <Description>
+##        Argument: <A>strip</A>, a strip
+##        <Br />
+##      </Description>
+##      <Returns
+##        &true; if <A>strip</A> is the zero strip of some SB algebra, and
+##        &false; otherwise.
+##      </Returns>
+##      <Description>
+##        Note that &sbstrips; knows which SB algebra <A>strip</A> belongs to.
+##        <Br />
+##      </Description>
 ##    </ManSection>
 ##  <#/GAPDoc>
 
@@ -65,43 +62,41 @@ DeclareProperty( "IsZeroStrip", IsStripRep );
 
 ##  <#GAPDoc Label="DocIsPeriodicStripByNthSyzygy">
 ##    <ManSection>
-##      <Oper Name="IsPeriodicStripByNthSyzygy" Arg="strip, N">
-##        <Description>
-##          Argument: <A>strip</A>, a strip; <A>N</A>, a positive integer
-##          <Br />
-##        </Description>
-##        <Returns>
-##          &true; if <A>strip</A> is appears among its own first <A>N</A>
-##          syzygies, and &false; otherwise.
-##        </Returns>
-##        <Description>
-##          If the call to this function returns &true;, then it will also
-##          print the index of the syzygy at which <A>strip</A> first appears.
-##          <Br />
-##        </Description>
-##      </Oper>
+##      <Oper Name="IsPeriodicStripByNthSyzygy" Arg="strip, N"/>
+##      <Description>
+##        Argument: <A>strip</A>, a strip; <A>N</A>, a positive integer
+##        <Br />
+##      </Description>
+##      <Returns>
+##        &true; if <A>strip</A> is appears among its own first <A>N</A>
+##        syzygies, and &false; otherwise.
+##      </Returns>
+##      <Description>
+##        If the call to this function returns &true;, then it will also print
+##        the index of the syzygy at which <A>strip</A> first appears.
+##        <Br />
+##      </Description>
 ##    </ManSection>
 ##  <#/GAPDoc>
 DeclareOperation( "IsPeriodicStripByNthSyzygy", [ IsStripRep, IsPosInt ] );
 
 ##  <#GAPDoc Label="DocIsFiniteSyzygyTypeStripByNthSyzygy">
 ##    <ManSection>
-##      <Oper Name="IsFiniteSyzygyTypeStripByNthSyzygy" Arg="strip, N">
-##        <Description>
-##          Argument: <A>strip</A>, a strip; <A>N</A>, a positive integer
-##          <Br />
-##        </Description>
-##        <Returns>
-##          &true; if the strips appearing in the <A>N</A>th syzygy of
-##          <A>strip</A> have all appeared among earlier syzygies, and &false;
-##          otherwise.
-##        </Returns>
-##        <Description>
-##          If the call to this function returns &true;, then it will also
-##          print the smallest <A>N</A> for which it would return &true;.
-##          <Br />
-##        </Description>
-##      </Oper>
+##    <Oper Name="IsFiniteSyzygyTypeStripByNthSyzygy" Arg="strip, N"/>
+##      <Description>
+##        Argument: <A>strip</A>, a strip; <A>N</A>, a positive integer
+##        <Br />
+##      </Description>
+##      <Returns>
+##        &true; if the strips appearing in the <A>N</A>th syzygy of
+##        <A>strip</A> have all appeared among earlier syzygies, and &false;
+##        otherwise.
+##      </Returns>
+##      <Description>
+##        If the call to this function returns &true;, then it will also print
+##        the smallest <A>N</A> for which it would return &true;.
+##        <Br />
+##      </Description>
 ##    </ManSection>
 ##  <#/GAPDoc>
 DeclareOperation(

2554ef236590170b67467d82c48402d8e89b1db4
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-10-12 12:44:22 +0100

    rename documentation-making functions

--- doc/makesbstripsdoc.gd
+++ doc/makesbstripsdoc.gd
@@ -1,7 +1,7 @@
 ##  Convenient functions that (re)make all the SBStrips documentation on my
 ##   computer
 DeclareGlobalFunction( "SBStripsDocumentationList" );
-DeclareGlobalFunction( "MakeSBStripsDocumentationTextAndLaTeXFiles" );
-DeclareGlobalFunction( "MakeSBStripsDocumentationSixAndHTMLFiles" );
+DeclareGlobalFunction( "MakeSBStripsDoc1" );
+DeclareGlobalFunction( "MakeSBStripsDoc2" );
 
 #########1#########2#########3#########4#########5#########6#########7#########

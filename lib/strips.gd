8c282181b7602293f89648fc84ebb4deb97be2dd
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-10-12 12:38:34 +0100

    document TestInjectivesStripsUpToNthSyzygy

--- lib/strips.gd
+++ lib/strips.gd
@@ -20,7 +20,7 @@ DeclareGlobalFunction( "StripifyVirtualStripNC" );
 ##      <Attr Name="SimpleStripsOfSbAlg", Arg="sba"/>
 ##      <Description>
 ##        Argument: <A>sba</A>, a special biserial algebra (ie, <Ref
-##        Label="IsSpecialBiserialAlgebra" Book="QPA"/> returs &true;)
+##        Prop="IsSpecialBiserialAlgebra" BookName="QPA"/> returs &true;)
 ##        <Br />
 ##      </Description>
 ##      <Returns>
@@ -41,7 +41,7 @@ DeclareAttribute( "SimpleStripsOfSbAlg", IsSpecialBiserialAlgebra );
 ##      <Attr Name="ProjectiveStripsOfSbAlg", Arg="sba"/>
 ##      <Description>
 ##        Argument: <A>sba</A>, a special biserial algebra (ie, <Ref
-##        Label="IsSpecialBiserialAlgebra" Book="QPA"/> returs &true;)
+##        Prop="IsSpecialBiserialAlgebra" BookName="QPA"/> returs &true;)
 ##        <Br />
 ##      </Description>
 ##      <Returns>
@@ -68,7 +68,7 @@ DeclareAttribute( "ProjectiveStripsOfSbAlg", IsSpecialBiserialAlgebra );
 ##      <Attr Name="InjectiveStripsOfSbAlg", Arg="sba"/>
 ##      <Description>
 ##        Argument: <A>sba</A>, a special biserial algebra (ie, <Ref
-##        Label="IsSpecialBiserialAlgebra" Book="QPA"/> returs &true;)
+##        Prop="IsSpecialBiserialAlgebra" BookName="QPA"/> returs &true;)
 ##        <Br />
 ##      </Description>
 ##      <Returns>
@@ -118,7 +118,6 @@ DeclareOperation( "WidthOfStrip", [ IsStripRep ] );
 ##      </Returns>
 ##      <Description>
 ##        Note that &sbstrips; knows which SB algebra <A>strip</A> belongs to.
-##        <Br />
 ##      </Description>
 ##    </ManSection>
 ##  <#/GAPDoc>
@@ -128,7 +127,7 @@ DeclareProperty( "IsZeroStrip", IsStripRep );
 ##    <ManSection>
 ##      <Oper Name="IsPeriodicStripByNthSyzygy" Arg="strip, N"/>
 ##      <Description>
-##        Argument: <A>strip</A>, a strip; <A>N</A>, a positive integer
+##        Arguments: <A>strip</A>, a strip; <A>N</A>, a positive integer
 ##        <Br />
 ##      </Description>
 ##      <Returns>
@@ -138,7 +137,6 @@ DeclareProperty( "IsZeroStrip", IsStripRep );
 ##      <Description>
 ##        If the call to this function returns &true;, then it will also print
 ##        the index of the syzygy at which <A>strip</A> first appears.
-##        <Br />
 ##      </Description>
 ##    </ManSection>
 ##  <#/GAPDoc>
@@ -148,7 +146,7 @@ DeclareOperation( "IsPeriodicStripByNthSyzygy", [ IsStripRep, IsPosInt ] );
 ##    <ManSection>
 ##    <Oper Name="IsFiniteSyzygyTypeStripByNthSyzygy" Arg="strip, N"/>
 ##      <Description>
-##        Argument: <A>strip</A>, a strip; <A>N</A>, a positive integer
+##        Arguments: <A>strip</A>, a strip; <A>N</A>, a positive integer
 ##        <Br />
 ##      </Description>
 ##      <Returns>
@@ -159,7 +157,6 @@ DeclareOperation( "IsPeriodicStripByNthSyzygy", [ IsStripRep, IsPosInt ] );
 ##      <Description>
 ##        If the call to this function returns &true;, then it will also print
 ##        the smallest <A>N</A> for which it would return &true;.
-##        <Br />
 ##      </Description>
 ##    </ManSection>
 ##  <#/GAPDoc>
@@ -168,7 +165,36 @@ DeclareOperation(
  [ IsStripRep, IsPosInt ]
  );
  
-# This is a temporary, informal function I intend to remove
-DeclareGlobalFunction( "TestInjectivesUpToNthSyzygy" );
+# This is a temporary, informal function I intend to remove (or rename?)
+##  <GAPDoc Label="DocTestInjectivesStripsUpToNthSyzygy">
+##    <ManSection>
+##      <Func Name="TestInjectivesStripsUpToNthSyzygy" Arg="sba, N"/>
+##      <Description>
+##        Arguments: <A>sba</A> a special biserial algebra (ie, <Ref
+##        Prop="IsSpecialBiserialAlgebra" BookName="QPA"/> returs &true;);
+##        <A>N</A>, a positive integer
+##        <Br />
+##      </Description>
+##      <Returns>
+##        &true;, if all strips of injective string modules have finite syzygy
+##        type by the <A>N</A>th syzygy, and &false; otherwise.
+##      </Returns>
+##      <Description>
+##        This function calls <Ref Attr="InjectiveStripsOfSbAlg"/> for
+##        <A>sba</A>, filters out all the &fail;s, and then checks each
+##        remaining strip individually using <Ref
+##        Oper="IsFiniteSyzygyTypeStripByNthSyzygy"/> (with second argument
+##        <A>N</A>).
+##        <P />
+##
+##        <E>Author's note.</E> For every special biserial algebra I test, this
+##        function returns true for sufficiently large <A>N</A>. It suggests
+##        that the injective cogenerator of a SB algebra always has finite
+##        syzygy type. This condition implies many homological conditions of
+##        interest (including the big finitistic dimension conjecture)!
+##      </Description>
+##    </ManSection>
+##  <#/GAPDoc>
+DeclareGlobalFunction( "TestInjectivesStripsUpToNthSyzygy" );
 
 #########1#########2#########3#########4#########5#########6#########7#########

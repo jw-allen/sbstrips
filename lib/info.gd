63d7185c390fb6b38ba76b815031555472fe0fda
jw-allen <66428493+jw-allen@users.noreply.github.com>
2020-11-30 09:58:01 +0000

    write documentation

--- lib/info.gd
+++ lib/info.gd
@@ -1,11 +1,14 @@
 ##  <#GAPDoc Label="DocInfoSBStrips">
-##  <InfoClass Name="InfoSBStrips"/>
-##  <Returns>
-##    nothing.
-##  </Returns>
-##  <Description>
-##    The InfoClass for the &SBStrips; package. The default value is <M>1</M>.
-##    Values up to <M>4</M> are supported.
-##  </Description>
+##  <ManSection>
+##    <InfoClass Name="InfoSBStrips" />
+##    <Returns>
+##      nothing.
+##    </Returns>
+##    <Description>
+##      The InfoClass for the &SBStrips; package. The default value is
+##      <M>1</M>. Values up to <M>4</M> are supported, offering increasingly
+##      verbose information about &SBStrips;' inner working.
+##    </Description>
+##  </ManSection>
 ##  <#/GAPDoc>
 DeclareInfoClass( "InfoSBStrips" );
